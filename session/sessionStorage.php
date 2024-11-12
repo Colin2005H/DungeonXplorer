b498bece78d2184b908d610dbc91412be91eaa0d
Alban <albangruyer@gmail.com>
2024-11-12 12:09:28 +0100

    start session storage

--- session/sessionStorage.php
+++ session/sessionStorage.php
@@ -1,16 +1,24 @@
 <?php
+
+
+
 session_start();
 
 #    $_SESSION DESCRIPTION
+#  
+#
+#   ["adventure"] => int
+#
+#   ["chapterID"] => int
+#
+#   ["character"] => Hero{}
 #   
-#   ["Character"]
-#   
-#   ["inventory"] =>{
+#   ["inventory"] =>Array{
 #       0:Item{
-#           itemID =>
-#           itemName =>
-#           itemDescrition =>
-#           itemQuantity =>   
+#           itemID => int
+#           itemName => string
+#           itemDescrition => string
+#           itemQuantity =>  int
 #           }
 #       1: ...
 #   }
@@ -18,14 +26,17 @@ session_start();
 
 
 class Session{
-
+    
     static function saveData(){
-
+        require_once './base/Database.php';
     }
 
-    static function loadData($playerID, $aventureID){
-        $playerID;
-        $aventureID;
+    static function loadData($heroID){
+        require_once './base/Database.php';
+        $_SESSION["chapterID"] = $db->query("SELECT chapter_id FROM quest WHERE hero_id = {$heroID};");
+
+        $_SESSION["adventure"] = $db->query("SELECT ad_id FROM chapter WHERE id = {$_SESSION["chapterID"]};");
+
     }
 
 
