cc0da252acc55345515836ecf3a32d51448f85e5
Alban <albangruyer@gmail.com>
2024-11-12 12:57:33 +0100

    fix base access

--- base/Database.php
+++ base/Database.php
@@ -1,31 +1,45 @@
 <?php
 
-// core/Database.php
+class Base{
 
-// Chemin vers le fichier .env
-$envFile = "../base/.env";
+    public $db;
 
-// Vérification de l'existence du fichier .env
-if (!file_exists($envFile)) {
-    die("Le fichier .env n'existe pas.");
+    public function __construct()
+    {
+        // Chemin vers le fichier .env
+        $envFile = "../base/.env";
+
+        // Vérification de l'existence du fichier .env
+        if (!file_exists($envFile)) {
+            die("Le fichier .env n'existe pas.");
+        }
+
+        // Lecture du fichier .env et récupération des variables d'environnement
+        $env = parse_ini_file($envFile);
+
+        // Récupération des variables d'environnement
+        $dbHost = $env['DB_HOST'];
+        $dbName = $env['DB_NAME'];
+        $dbUser = $env['DB_USER'];
+        $dbPassword = $env['DB_PASSWORD'];
+
+        try {
+            // Connexion à la base de données
+            $this->db = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
+            // Définition des attributs de PDO pour afficher les erreurs
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            echo "Erreur de connexion à la base de données : " . $e->getMessage();
+            exit;
+        }
+    }
+        
+    public function request($requestString){
+        $res = $this->db->prepare($requestString);
+        $res->execute();
+        return $res->fetchAll();
+    }
 }
 
-// Lecture du fichier .env et récupération des variables d'environnement
-$env = parse_ini_file($envFile);
+$base = new Base();
 
-// Récupération des variables d'environnement
-$dbHost = $env['DB_HOST'];
-$dbName = $env['DB_NAME'];
-$dbUser = $env['DB_USER'];
-$dbPassword = $env['DB_PASSWORD'];
-$db;
-
-try {
-    // Connexion à la base de données
-    $db = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
-    // Définition des attributs de PDO pour afficher les erreurs
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Erreur de connexion à la base de données : " . $e->getMessage();
-    exit;
-}
