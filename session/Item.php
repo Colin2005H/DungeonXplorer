bcdcff95fda04aa43b75b2e4f90f25debf45c0db
Alban <albangruyer@gmail.com>
2024-11-24 15:29:17 +0100

    class Item finished

--- session/Item.php
+++ session/Item.php
@@ -1,26 +1,29 @@
 <?php
 
-abstract class Item {
+class Item {
 
-    public $itemID;
-    public $itemName;
-    public $itemDescription;
-    public $itemQuantity;
+    public $id;
+    public $name;
+    public $description;
+    public $quantity;
+    public $weight;
 
-    public function __construct($itemID, $itemName, $itemDescription)
+    public function __construct($id, $name, $description, $weight)
     {
-        $this->itemID = $itemID;
-        $this->itemName = $itemName;
-        $this->itemDescription = $itemDescription;
-        $this->itemQuantity = 1;
+        $this->id = $id;
+        $this->name = $name;
+        $this->description = $description;
+        $this->weight = $weight;
+        $this->quantity = 1;
     }
 
-    public function increaseStack($val){
-        $this->itemQuantity + $val;
+    public function getTotalWeight(){
+        return $this->quantity * $this->weight;
     }
 
-    public function decreaseStack($val){
-        $this->itemQuantity - $val;
+    public static function getItem($id):Item{
+        $itemInfo = $GLOBALS["base"]->request("SELECT * FROM Items WHERE id = {$id}")[0];
+        return new Item($id, $itemInfo["name"], $itemInfo["description"], $itemInfo["weight"]);
     }
 
 }
