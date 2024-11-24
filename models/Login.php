523797c361afb5d5c0484693e5cdfcb2e35bd64a
Alban <albangruyer@gmail.com>
2024-11-24 16:30:12 +0100

    login use new global base

--- models/Login.php
+++ models/Login.php
@@ -5,16 +5,14 @@ class Login
 
     public static function login($username, $password): int{
         require_once '../base/Database.php';
-        $res = $base->request("SELECT count(*) as connection FROM User WHERE user_pseudo = '{$username}' and user_password = '{$password}'");
-        return $res[0]['connection'];
+        return $GLOBALS["base"]->request("SELECT count(*) as connection FROM User WHERE user_pseudo = '{$username}' and user_password = '{$password}'")[0]['connection'];
     }
 }
 
-    if(Login::login($_POST['username'], $_POST['password'])){
-        #connection succed
-        echo "connection (TODO rediriger vers choix personnage)";
-    }else{
-        #connection failed
-        echo "echec (TODO rediriger vers home)";
-    }
-    
+if(Login::login($_POST['username'], $_POST['password'])){
+    #connection succed
+    echo "connection (TODO rediriger vers choix personnage)";
+}else{
+    #connection failed
+    echo "echec (TODO rediriger vers home)";
+}
