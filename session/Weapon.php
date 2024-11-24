ec7bccd1c897dc3e1a4b679f6183c69a48cda930
Alban <albangruyer@gmail.com>
2024-11-24 16:01:56 +0100

    class Weapon finished

--- session/Weapon.php
+++ session/Weapon.php
@@ -2,15 +2,26 @@
 
 class Weapon extends Item{
 
+    public $id;
     public $wptID;
     public $bonusStreagth;
+    public $initiativePenalty;
 
-    public function __construct($itemID, $itemName, $itemDescription, $wptID, $bonusStreagth){
-        Item::__construct($itemID, $itemName, $itemDescription);
+
+    public function __construct($itemId, $itemName, $itemDescription, $itemWeight, $id, $wptID, $bonusStreagth, $initiativePenalty){
+        Item::__construct($itemId, $itemName, $itemDescription, $itemWeight);
         
+        $this->id = $id;
         $this->wptID = $wptID;
         $this->bonusStreagth = $bonusStreagth;
+        $this->initiativePenalty = $initiativePenalty;
         
+    }
+
+    public static function getWeapon($id):Weapon{
+        $weaponInfo = $GLOBALS["base"]->request("SELECT * FROM Weapon WHERE wp_id = {$id}")[0];
+        $itemInfo = $GLOBALS["base"]->request("SELECT * FROM Items WHERE id = {$weaponInfo["item_id"]}")[0];
+        return new Weapon($itemInfo["id"], $itemInfo["name"], $itemInfo["description"], $itemInfo["weight"], $id, $weaponInfo["wpt_id"], $weaponInfo["bonus_strength"], $weaponInfo["initiative_penalty"]);
     }
 
 }
