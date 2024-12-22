5d9668b5999682437c1c0e6f728d387afae38299
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2024-12-23 00:09:40 +0100

    new database

--- base/Database.php
+++ base/Database.php
@@ -7,15 +7,17 @@ class Base{
     public function __construct(){
   
         // Récupération des variables d'environnement
-        $dbHost = "mysql-etu.unicaen.fr";
-        $dbName = "legoupi231_0";
-        $dbUser = "legoupi231";
-        $dbPassword = "rahgho1os0ieDeix";
+        $dbHost = "dunjeonxplorer-dunjeonxplorer.g.aivencloud.com";
+        $dbPort = "27061";
+        $dbName = "defaultdb";
+        $dbUser = "avnadmin";
+        $dbPassword = "AVNS_yA4K7tJHMO3MqRheWWN";
 
         try {
             // Connexion à la base de données
-            $this->db = new PDO("mysql:host=$dbHost;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
+            $this->db = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
             // Définition des attributs de PDO pour afficher les erreurs
+            echo "Succès";
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Erreur de connexion à la base de données : " . $e->getMessage();
