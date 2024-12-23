5b69c6ba2864ada84f913228d1b1ff982c22b7ac
Alban <albangruyer@gmail.com>
2024-12-23 18:24:24 +0100

    fix sign in

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -11,18 +11,19 @@ class UserController {
 
     public function testLogin(){
         require_once './base/Database.php';
-        print_r($_POST["username"]);
         $logSuccess = $GLOBALS["base"]->request("SELECT count(*) as connection FROM User WHERE user_pseudo = '{$_POST["username"]}' and user_password = '{$_POST["password"]}'")[0]['connection'];
-
+        
         if($logSuccess){
             #connection succed
             echo "connection (TODO rediriger vers choix personnage)";
         }else{
             #connection failed
-            require_once 'views/signin.php';
+            echo "connection failed";
+            //require_once 'views/signin.php';
             
 
         }
+        
     }
    
     
