2c8bbe602ff3fc8c21209a5eb555fdb6b6056e90
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2024-12-23 11:49:40 +0100

    database connection logic and improve error handling

--- base/Database.php
+++ base/Database.php
@@ -5,22 +5,15 @@ class Base{
     public $db;
 
     public function __construct(){
-  
-        // Récupération des variables d'environnement
-        $dbHost = "dunjeonxplorer-dunjeonxplorer.g.aivencloud.com";
-        $dbPort = "27061";
-        $dbName = "defaultdb";
-        $dbUser = "avnadmin";
-        $dbPassword = "AVNS_yA4K7tJHMO3MqRheWWN";
-
         try {
             // Connexion à la base de données
-            $this->db = new PDO("mysql:host=$dbHost;port=$dbPort;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
-            // Définition des attributs de PDO pour afficher les erreurs
-            echo "Succès";
+            $this->db = new PDO('mysql:host=dunjeonxplorer-dunjeonxplorer.g.aivencloud.com:27061;dbname=DunjeonXplorer;charset=utf8', 'avnadmin', 'AVNS_yA4K7tJHMO3MqRheWWN');
             $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Connexion à la base de données réussie";
+            echo "<script>console.log('Connexion à la base de données réussie');</script>";
         } catch (PDOException $e) {
             echo "Erreur de connexion à la base de données : " . $e->getMessage();
+            echo "<script>console.log('Erreur de connexion à la base de données : " . $e->getMessage() . "');</script>";
             exit;
         }
     }
