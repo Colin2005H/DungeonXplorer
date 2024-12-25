c53adcc238d235785c36c889982abf5bbdd2d594
Colin <colin@example.com>
2024-12-25 22:48:51 +0100

    Update to the chapter style

--- views/homePage.php
+++ views/homePage.php
@@ -20,12 +20,24 @@
         <div class= "bigLogo">
             <img  id = "logoHomePage" src="./views/images/Logo.png" alt="Dungeon Xplorer logo" />
         </div>
-        <h1>Welcome to Dungeon Xplorer</h1>
-        <p id = "Introduction">Are you ready to explore the dungeons?</p>
+        <h1>Bienvenue sur Dungeon Xplorer</h1>
+        <p id = "Introduction">DungeonXplorer, l'univers de dark fantasy où se mêlent aventure, stratégie et immersion
+totale dans les récits interactifs.</p>
         <div class="SignIn_SignOut">
             <a href="login" class="button">Sign in</a>
             <a href="signup" class="button">Sign up</a>
         </div>
+        <p class="edito">
+    Ce projet est né de la volonté de l’association "Les Aventuriers du Val Perdu" de raviver l’expérience unique<br>
+    des livres dont vous êtes le héros. Notre vision : offrir à la communauté un espace où chacun peut<br>
+    incarner un personnage et plonger dans des quêtes épiques et personnalisées.<br>
+    Dans sa première version, DungeonXplorer permettra aux joueurs de créer un personnage parmi trois<br>
+    classes emblématiques — guerrier, voleur, magicien — et d’évoluer dans un scénario captivant, tout en<br>
+    assurant à chacun la possibilité de conserver sa progression.<br>
+    Nous sommes enthousiastes de partager avec vous cette application et espérons qu'elle saura vous<br>
+    plonger au cœur des mystères du Val Perdu !
+        </p>
+
     </div>
 
     
