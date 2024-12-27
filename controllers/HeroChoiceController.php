c2097a8b6b0cfa7319a8c4dc133291bccc989f2e
Alban <albangruyer@gmail.com>
2024-12-27 23:30:27 +0100

    hero creation allmost complete

--- controllers/HeroChoiceController.php
+++ controllers/HeroChoiceController.php
@@ -55,19 +55,31 @@ class HeroChoiceController {
 
     public function create(){
 
-        $name = isset($_POST['name']) ? htmlspecialchars(trim($_POST['name'])) : '';
-        $biographie = isset($_POST['email']) ? htmlspecialchars(trim($_POST['email'])) : '';
-        $classid = isset($_POST['classe']) ? htmlspecialchars(trim($_POST['classe'])) : '';
-        if (empty($name) || empty($classid)) {
-            $this->show();
-        }else {
-            // Exemple de traitement : affichage des données
-            $base = $GLOBALS["base"];
-            $class = $base->request("SELECT * FROM class WHERE id = $classid");
-            $base->request("INSERT INTO Hero (name,class_id,biographie,pv,mana,strenght,initiative,shield,xp,current_level,user_id,am_id,primary_wp_id,secondary_wp_id,wheight_limit,qte_item_limit) VALUES ($name,$classid,$biographie,{$class['base_pv']},{$class['base_mana']},{$class['base_strenght']},{$class['base_initiative']},{$class['base_shield']},0,1,{$_SESSION[user_id]}  ,{$class['base_armor']},{$class['base_primary_wp']},{$class['base_secondary_wp']},{$class['base_wheight_limit']},{$class['base_qte_item_limit']})");
         
-            // Vous pouvez aussi enregistrer ces données dans une base de données
-    
+        
+        
+        if (!isset($_POST['name']) || !isset($_POST['classe'])) {
+            
+            header('Location: herocreation');
+            exit;
+
+        }else {
+            $name = trim($_POST['name']);
+            $classid = trim($_POST['classe']);
+            $biographie = "NULL";
+
+            if(isset($_POST['biographie'])){
+                $biographie = trim($_POST['email']);
+            }
+            $base = $GLOBALS["base"];
+            $base->request("INSERT INTO Hero (name,class_id,biographie,pv,mana,strenght,initiative,shield,xp,current_level,user_id,am_id,primary_wp_id,secondary_wp_id,wheight_limit,qte_item_limit) VALUES ($name,$classid,$biographie,{$class['base_pv']},{$class['base_mana']},{$class['base_strenght']},{$class['base_initiative']},{$class['base_shield']},0,1,{$_SESSION[user_id]}  ,{$class['base_armor']},{$class['base_primary_wp']},{$class['base_secondary_wp']},{$class['base_wheight_limit']},{$class['base_qte_item_limit']})");
+            
+            $_SESSION[''];
+            $_SESSION['inventory'] = serialize(new Inventory([], [], []));
+            $_SESSION['chapter'] = $GLOBALS['base']->request("SELECT * FROM Adventure WHERE ad_id = {$this->adventure}")[0]['chapter_id'];
+            
+            header('Location: chapter/'.$this->adventure);
+            exit;
         }
     }
 
